0ffa67ec4b13849f65941743a1c54b503e50cec1
sevvalkapcak <sevvalkapcak@gmail.com>
2023-06-02 15:48:56 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Jupyter Notebook dosyanızı çalıştırma
+subprocess.run(['jupyter', 'nbconvert', '--execute', 'notebook.ipynb'])
+
+# Raporlama veya test senaryolarını ekleyebilirsiniz
+# Testlerinizin sonuçlarını raporlayabilir veya başka işlemler gerçekleştirebilirsiniz
